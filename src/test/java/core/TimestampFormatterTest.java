676f1a8309edd638a16951faca10f3a5764110e4
David Fialho <fialho.david@gmail.com>
2017-01-31 16:07:24 +0000

    Fixed test on the format() method of the TimestampFormatter

--- src/test/java/core/TimestampFormatterTest.java
+++ src/test/java/core/TimestampFormatterTest.java
@@ -55,7 +55,7 @@ public class TimestampFormatterTest {
     public void format_ValidFormattedString_CorrespondingTimestamp() throws Exception {
 
         assertThat(formatter.format(Timestamp.of(2016, 1, 9, 5, 3, 2)),
-                is("09/01/2016 05:03:03"));
+                is("09/01/2016 05:03:02"));
     }
 
 }
