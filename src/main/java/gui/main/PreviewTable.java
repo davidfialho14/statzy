3615bffcbb599d53124ecbcab0a664cdb92c0a92
David Fialho <fialho.david@gmail.com>
2017-02-04 10:50:46 +0000

    Implemented the logic to select and un-select the date, time, and ignored columns

--- src/main/java/gui/main/PreviewTable.java
+++ src/main/java/gui/main/PreviewTable.java
@@ -1,15 +1,21 @@
 package gui.main;
 
+import com.sun.javafx.collections.ObservableSetWrapper;
 import core.Record;
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.ObservableSet;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.ToggleButton;
 import javafx.scene.layout.GridPane;
 
 import java.io.IOException;
+import java.util.HashSet;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -25,6 +31,9 @@ public class PreviewTable extends GridPane {
     
     private boolean dataIsSet = false;
     private boolean headersAreSet = false;
+    private IntegerProperty dateColumn= new SimpleIntegerProperty(0);
+    private IntegerProperty timeColumn = new SimpleIntegerProperty(1);
+    private ObservableSet<Integer> ignoredColumns = new ObservableSetWrapper<>(new HashSet<>());
 
     public PreviewTable() {
 
@@ -44,11 +53,49 @@ public class PreviewTable extends GridPane {
      * Gives the table column an index that can be used by the cell value factory to to get the correct value
      * from the data model. Adds a context menu to set the column as a date, time or datetime column. Adds a
      */
-    public static class PreviewTableColumn extends TableColumn<Record, String> {
+    public class PreviewTableColumn extends TableColumn<Record, String> {
+
+        private final Button button;
+        private final int index;
 
         public PreviewTableColumn(String header, int index) {
             super();
-            setText(header);
+            this.index = index;
+
+            button = new Button(header);
+            button.setMaxWidth(Double.MAX_VALUE);   // ensure button stretches for the entire column width
+            button.setStyle("-fx-background-color:transparent;");   // make button transparent
+
+            // Setup the logic to selecting the date, time, and ignored columns
+
+            button.setOnMouseClicked(event -> {
+
+                if (dateToggle.isSelected()) {
+                    selectSingleColumn(dateColumn, dateToggle);
+                    System.out.println("date: " + dateColumn);
+
+                } else if (timeToggle.isSelected()) {
+                    selectSingleColumn(timeColumn, timeToggle);
+                    System.out.println("time: " + timeColumn);
+
+                } else if (ignoreToggle.isSelected()) {
+
+                    // select columns as ignored only if this is not the date and/or time column
+                    if (index != dateColumn.getValue() && index != timeColumn.getValue()) {
+
+                        // if the column is already ignored then this makes the column "un-ignored"
+                        // if the column was not ignored then it becomes ignored
+                        if (!ignoredColumns.remove(index)) {
+                            ignoredColumns.add(index);
+                        }
+                    }
+
+                    System.out.println("ignored: " + ignoredColumns);
+                }
+
+            });
+
+            setGraphic(button);
 
             // disable sorting and resizing the columns
             setSortable(false);
@@ -56,6 +103,21 @@ public class PreviewTable extends GridPane {
             // since each row stores a list, the cell value for each column is the item in the position given
             // by the column index
             setCellValueFactory(param -> new SimpleStringProperty(param.getValue().get(index)));
+        }
+
+        public void setHeader(String header) {
+            button.setText(header);
+        }
+
+        private void selectSingleColumn(IntegerProperty columnIndex, ToggleButton toggle) {
+            // update the index to the index of this column
+            columnIndex.setValue(index);
+            // if the column was being ignored now it is not
+            ignoredColumns.remove(columnIndex.getValue());
+
+            // un-select the toggle automatically
+            // This is a good solution since the user can only select column with this toggle
+            toggle.setSelected(false);
         }
 
     }
@@ -75,7 +137,7 @@ public class PreviewTable extends GridPane {
 
             if (headers.size() != tableView.getColumns().size()) {
                 // clear headers from all columns
-                tableView.getColumns().forEach(column -> column.setText(""));
+                tableView.getColumns().forEach(column -> preview(column).setHeader(""));
 
                 throw new PreviewException("Headers does not correspond to current data file: headers file " +
                         "has " + headers.size() + " headers and the data file has " +
@@ -83,7 +145,7 @@ public class PreviewTable extends GridPane {
             }
 
             for (int i = 0; i < headers.size(); i++) {
-                tableView.getColumns().get(i).setText(headers.get(i));
+                preview(tableView.getColumns().get(i)).setHeader(headers.get(i));
             }
 
         } else {
@@ -171,6 +233,10 @@ public class PreviewTable extends GridPane {
         }
 
         dataIsSet = false;
+    }
+
+    private static PreviewTableColumn preview(TableColumn<Record, ?> column) {
+        return (PreviewTableColumn) column;
     }
 
 }
